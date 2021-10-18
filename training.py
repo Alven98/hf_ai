c0a2aafbd7dde70b52c97db062650732f977dccd
alvenleong98 <alvenleong67@gmail.com>
2021-10-18 20:18:05 +0800

    Combine First NN Model with DDQN

--- training.py
+++ training.py
@@ -1,68 +1,96 @@
 import numpy as np
 import os
+import math
 
 from ddqn import Agent
 from utils import plot_learning_curve
 from update_design import HFSS
+
+from neural_network import NN
+
+
+def form_parameters(num_mclin, initial_values):
+    half = int(math.ceil(num_mclin / 2))
+    rest = int(num_mclin - half)
+    values = initial_values[0]
+    total = len(values)
+    divides = int(total / 2)
+
+    parameters = {}
+    cnt = 1
+    for i in range(half):
+        key_name = 'MCLIN' + str(cnt)
+        mclin_dict = {
+            'W': values[i],
+            'S': values[divides + i]
+        }
+        cnt += 1
+        parameters[key_name] = mclin_dict
+
+    for i in range(rest - 1, -1, -1):
+        key_name = 'MCLIN' + str(cnt)
+        mclin_dict = {
+            'W': values[i],
+            'S': values[divides + i]
+        }
+        cnt += 1
+        parameters[key_name] = mclin_dict
+
+    return parameters
 
 
 if __name__ == '__main__':
 
     fps_path = os.path.join(os.getcwd(), r'Test\fpx\ParallelCoupledline_2.fpx')
     snp_path = os.path.join(os.getcwd(), r'Test\s2p\ParallelCoupledline_2.s2p')
+    dpath = r"datasets/mclin.xlsx"
 
-    num_mclin = 5
-    tuning_parameter = ["W", "S"]
-    mclin_w_min = 2e-3
-    mclin_w_max = 3e-3
-    mclin_s_min = 0.2e-3
-    mclin_s_max = 0.3e-3
-    cf = 0.5
-    el = 90
+    sample = {
+        'fo': 4.7e9,
+        'bandwidth': 2.8e8,
+        'length': 0.0117,
+        'mclin': 5
+    }
+    nn = NN(dpath, sample)
+    initial_guess = nn.predict_nn()
 
-    # Predicted From First Model (TODO: Predict W, S, CF, EL from First Model CNN)
-    for num in range(num_mclin):
-        mclin_id = num + 1
-        if num in [0, 4]:
-            mclin_s_min = 0.2e-3
-            mclin_s_max = 0.3e-3
-            cf = 0.5894
-            el = 90.6
-        elif num in [1, 3]:
-            mclin_s_min = 2e-3
-            mclin_s_max = 3e-3
-            cf = 0.876992
-            el = 90.69
-        else:
-            mclin_s_min = 2e-3
-            mclin_s_max = 3e-3
-            cf = 0.901332
-            el = 90.69
+    params = form_parameters(sample['mclin'], initial_guess)
+    half_cntr = int(math.ceil(sample['mclin'] / 2))
+    rest_cntr = sample['mclin'] - half_cntr
+    keys = list(params.keys())
 
-        mclin_w_range = (mclin_w_min, mclin_w_max)
-        mclin_s_range = (mclin_s_min, mclin_s_max)
+    # Predicted From First Model
+    for num in range(half_cntr):
+        mclin_ids = [keys[num], keys[len(keys) - 1 - num]]
+        mclin_params = params[mclin_ids[0]]
+        mclin_w_range = (mclin_params['W'] * 0.9, mclin_params['W'] * 1.1)
+        mclin_s_range = (mclin_params['S'] * 0.9, mclin_params['S'] * 1.1)
+        tuning_parameter = list(mclin_params.keys())
 
         for tune_para in tuning_parameter:
-            env = HFSS(fps_path, snp_path, mclin_w_range, mclin_s_range, cf, el, tune_para, mclin_id)
+            env = HFSS(fpx_path=fps_path, snp_path=snp_path, mclin_w_range=mclin_w_range, mclin_s_range=mclin_s_range,
+                       fo=sample['fo'], bandwidth=sample['bandwidth'], length=sample['length'], mclin_ids=mclin_ids,
+                       initial_guess=params, tuning_parameter=tune_para, return_loss=-10, insertion_loss=-50)
+
             num_action = len(env.action)
             num_state = len(env.state)
-            print(f"*****  Initializing Fine-Tuning on {tune_para} Parameter for MCLIN{mclin_id}  *****".format(tune_para=tune_para, mclin_id=mclin_id))
+            print(f"*****  Initializing Fine-Tuning on {tune_para} Parameter for {mclin_ids}  *****".format(tune_para=tune_para, mclin_ids=str(mclin_ids)))
             print(f"no. of state: {num_state} | no. of action: {num_action}".format(num_state=num_state, num_action=num_action))
 
-            n_episodes = 50
+            n_episodes = 100
             load_checkpoint = False
 
             agent = Agent(gamma=0.99, epsilon=1.0, eps_min=0.01, eps_dec=1e-3, alpha=5e-4, input_dims=num_state,
-                          n_actions=num_action, mem_size=1000000, batch_size=64, tune_parameter=tune_para, mclin_id=mclin_id)
+                          n_actions=num_action, mem_size=1000000, batch_size=64, tune_parameter=tune_para, mclin_ids=str(mclin_ids))
 
             if load_checkpoint:
                 agent.load_model()
 
             plot_loss_graph = 'Tuning-loss.png'
             if tune_para == "W":
-                plot_loss_graph = os.path.join(os.getcwd(), r'ddqn/loss analysis/MCLIN' + str(mclin_id) + '_Width_Tuning-loss.png')
+                plot_loss_graph = os.path.join(os.getcwd(), r'ddqn/loss analysis/' + str(mclin_ids) + '_Width_Tuning-loss.png')
             if tune_para == "S":
-                plot_loss_graph = os.path.join(os.getcwd(), r'ddqn/loss analysis/MCLIN' + str(mclin_id) + '_Space_Tuning-loss.png')
+                plot_loss_graph = os.path.join(os.getcwd(), r'ddqn/loss analysis/' + str(mclin_ids) + '_Space_Tuning-loss.png')
             scores, eps_history = [], []
 
             for ep in range(n_episodes):
@@ -91,5 +119,8 @@ if __name__ == '__main__':
 
             x = [i+1 for i in range(n_episodes)]
             plot_learning_curve(x, scores, eps_history, plot_loss_graph)
-            print(f"*****  MCLIN{mclin_id} - {tune_para} Estimation Model Training completed *****".format(mclin_id=mclin_id, tune_para=tune_para))
+            params[mclin_ids[0]][tune_para] = env.value_update
+            params[mclin_ids[1]][tune_para] = env.value_update
+            print(f"*****  {mclin_ids} - {tune_para} Estimation Model Training completed *****".format(mclin_ids=str(mclin_ids), tune_para=tune_para))
             print("")
+    print(params)
